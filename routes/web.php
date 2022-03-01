acb1fd64f7acd43ca99be2beb8ba127010336d2c
Siulacio <siulacio@hotmail.com>
2022-03-01 11:19:11 -0500

    feat: order and showcase routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\OrderController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+    Route::get('/', function () {
+        return view('welcome');
+    });
+*/
+/*
+    Route::get('/comprar', function(){
+        return view('shop.create_order');
+    });
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ShopController::class, 'index'])->name('product.showcase');
+Route::get('/order/create/{product}',[OrderController::class, 'create'])->name('order.create');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
