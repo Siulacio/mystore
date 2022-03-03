fae16068b49580d89d8af1a74efae1a86a70fc66
Siulacio <siulacio@hotmail.com>
2022-03-03 17:46:04 -0500

    feat: order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,14 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 use App\Models\Product;
+use App\Models\Order;
+use App\Http\Requests\StoreOrderRequest;
 
 class OrderController extends Controller
 {
-    public function create($product_id) : View
+
+    public function index() : View
     {
-        $product = Product::find($product_id);
+
+        $orders = Order::with(['products'])->get();
+        return view('shop.list_orders', compact('orders'));
+    }
+
+    public function create(Product $product) : View
+    {
         return view('shop.create_order',compact('product'));
+    }
+
+    public function store(StoreOrderRequest $request)
+    {
+        $order = Order::create([
+            'customer_name' => $request->customer_name,
+            'customer_email' => $request->customer_email,
+            'customer_mobile' => $request->customer_mobile,
+            'product_id' => $request->product_id,
+        ]);
+
+        return redirect('/order/checkout/'.$order->id);
+
+    }
+
+    public function checkout(Order $order) : View
+    {
+        return view('shop.checkout', compact('order'));
     }
 }
