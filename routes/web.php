4a654d9d565d4d15456782b28c2278d3199d8619
Siulacio <siulacio@hotmail.com>
2022-03-03 16:56:20 -0500

    feat: order routes

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PaymentController;
 
 /*
     Route::get('/', function () {
         return view('welcome');
     });
 */
-/*
-    Route::get('/comprar', function(){
-        return view('shop.create_order');
+
+    Route::get('/check', function(){
+        return view('shop.checkout');
     });
-*/
+
 
 Route::get('/',[ShopController::class, 'index'])->name('product.showcase');
+Route::get('/order',[OrderController::class, 'index'])->name('order.index');
 Route::get('/order/create/{product}',[OrderController::class, 'create'])->name('order.create');
+Route::post('/order/store',[OrderController::class, 'store'])->name('order.store');
+Route::get('/order/checkout/{order}',[OrderController::class, 'checkout'])->name('order.checkout');
+Route::post('/payment/make',[PaymentController::class,'createPayment']);
+Route::get('/ruta/ejemplo/{id}',[PaymentController::class,'getRoute']);
+//Route::get('/payment/{order}',[PaymentController::class,'createPayment']); //viene redirigido al crear la orden
 
 
 Auth::routes();
